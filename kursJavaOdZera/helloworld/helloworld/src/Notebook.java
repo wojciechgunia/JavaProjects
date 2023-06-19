39d5ab0a56f96f990b399265ef27dc9d8d2fbad1
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-19 14:19:01 +0200

    JavaOdZera - HelloWorld/Zmienne - Notebook add

---
+++ kursJavaOdZera/helloworld/helloworld/src/Notebook.java
@@ -0,0 +1,22 @@
+public class Notebook {
+
+    public static void main(String[] args)
+    {
+        //zmienne
+
+        int x = 5;  //x = 5
+        int y = 10; //y = 10
+
+        int sum = x+y; //sum = 5 + 10
+
+        x=y; //x = y = 10
+
+        System.out.println(sum);
+        System.out.println(x);
+
+        String zdanie = "Ala" + " ma " + "kota"; // koniunkcja łańcuchów znakowych
+        System.out.println(zdanie);
+
+    }
+
+}
