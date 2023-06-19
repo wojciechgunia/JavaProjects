1d82179d46e5ac29d90e4bab47d174d9d273738f
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-19 16:52:32 +0200

    JavaOdZera - Funkcje - Calculator 1.1

--- kursJavaOdZera/funkcje/src/Calculator.java
+++ kursJavaOdZera/funkcje/src/Calculator.java
@@ -3,10 +3,28 @@ public class Calculator
     public static void main(String[] args)
     {
         System.out.println(sum(42, 2.6));
+        System.out.println(sub(42, 2.6));
+        System.out.println(mul(42, 2.6));
+        System.out.println(div(42, 2.6));
     }
 
     public static double sum(double a, double b)
     {
         return a + b;
+    }
+
+    public static double sub(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double mul(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double div(double a, double b)
+    {
+        return a / b;
     }
 }
