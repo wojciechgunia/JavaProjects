5e91e51ad20126721b215ac1e494e1ff07999c92
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-19 18:27:44 +0200

    JavaOdZera - Notebook - Instrukcje warunkowe if,if else,else

--- kursJavaOdZera/Inne tematy i notebook/src/notebook/Notebook.java
+++ kursJavaOdZera/Inne tematy i notebook/src/notebook/Notebook.java
@@ -9,11 +9,17 @@ public class Notebook {
         int x = 5;  //x = 5
         int y = 10; //y = 10
 
-        int sum = x+y; //sum = 5 + 10
+        int sum = x+y; //sum = 5 + 10 = 15
+        int sub = x-y; //sum = 5 - 10 = -5
+        int mul = x*y; //sum = 5 * 10 = 50
+        float div = (float) x /y; //sum = 5 / 10 = 0.5
 
         //x=y; //x = y = 10
 
         System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
         System.out.println(x);
 
         String zdanie = "Ala" + " ma " + "kota"; // koniunkcja łańcuchów znakowych
@@ -30,6 +36,25 @@ public class Notebook {
             MALE, FEMALE
         }/
         */
+
+        // modulo
+        System.out.println(16%5); // reszta z dzielenia 16 przez 5
+
+        // if, else if, else
+        if(x>y)
+        {
+            System.out.println(x);
+        }
+        else if(x==y)
+        {
+            System.out.println(x);
+        }
+        else
+        {
+            System.out.println(y);
+        }
+
+        //
     }
 
     //funkcje
