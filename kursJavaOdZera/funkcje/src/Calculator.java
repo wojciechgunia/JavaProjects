5096990a8b062499b37f19726dbe4e7fe20dd2e2
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-19 16:39:09 +0200

    JavaOdZera - Funkcje - Calculator 1.0

---
+++ kursJavaOdZera/funkcje/src/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        System.out.println(sum(42, 2.6));
+    }
+
+    public static double sum(double a, double b)
+    {
+        return a + b;
+    }
+}
