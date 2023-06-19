0f04849175597054acca563acb4a09d0d209a8e1
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-19 13:54:31 +0200

    JavaOdZera - HelloWorld/Zmienne2

---
+++ kursJavaOdZera/helloworld/helloworld/src/Zmienne.java
@@ -0,0 +1,29 @@
+public class Zmienne
+{
+    public static void main(String[] args)
+    {
+        String name = "Wojciech";
+        int age = 21;
+        boolean study = true;
+
+        String name2 = "Ola";
+        int age2 = 19;
+        boolean study2 = false;
+
+        String name3 = "Adam";
+        int age3 = 20;
+        boolean study3 = true;
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Student: "+study+"\n");
+
+        System.out.println("Name: "+name2);
+        System.out.println("Age: "+age2);
+        System.out.println("Student: "+study2+"\n");
+
+        System.out.println("Name: "+name3);
+        System.out.println("Age: "+age3);
+        System.out.println("Student: "+study3+"\n");
+    }
+}
