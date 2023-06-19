136c38164f2da90bb2cd8985d0732f32136e9be8
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-19 16:56:26 +0200

    JavaOdZera - Funkcje - singASong 1.0

--- kursJavaOdZera/funkcje/src/Functions.java
+++ kursJavaOdZera/funkcje/src/Functions.java
@@ -5,13 +5,18 @@ public class Functions
         System.out.println(showPerson("Wojciech",21,true));
         System.out.println(showPerson("Anna",19,false));
         System.out.println(showPerson("Adam",20,true));
+        System.out.println(singASong("Ala","kota"));
     }
 
     public static String showPerson(String name,int age,boolean study)
     {
-        //String info = "Name: "+name+"\n"+"Age: "+age+"\n"+"Student: "+study+"\n\n";
-        //System.out.println(info);
+        //String info = "Name: "+name+"\n"+"Age: "+age+"\n"+"Student: "+study+"\n\n"
+        //System.out.println(info)
         return "Name: "+name+"\n"+"Age: "+age+"\n"+"Student: "+study+"\n";
     }
 
+    public static String singASong(String person, String pet)
+    {
+        return person + " ma " + pet;
+    }
 }
