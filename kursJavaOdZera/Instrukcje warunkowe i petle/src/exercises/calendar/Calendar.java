6a595912c3417dcf756906066fec23fd027473cf
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-20 12:26:04 +0200

    JavaOdZera - Instrukcje warunkowe - exercise Calendar - switch

--- kursJavaOdZera/Instrukcje warunkowe i petle/src/exercises/calendar/Calendar.java
+++ kursJavaOdZera/Instrukcje warunkowe i petle/src/exercises/calendar/Calendar.java
@@ -10,57 +10,34 @@ public class Calendar
 
     public static month getMonth(int m)
     {
-        if(m==1)
+        switch(m)
         {
-            return month.JANUARY;
-        }
-        else if(m==2)
-        {
-            return month.FEBRUARY;
-        }
-        else if(m==3)
-        {
-            return month.MARCH;
-        }
-        else if(m==4)
-        {
-            return month.APRIL;
-        }
-        else if(m==5)
-        {
-            return month.MAY;
-        }
-        else if(m==6)
-        {
-            return month.JUNE;
-        }
-        else if(m==7)
-        {
-            return month.JULY;
-        }
-        else if(m==8)
-        {
-            return month.AUGUST;
-        }
-        else if(m==9)
-        {
-            return month.SEPTEMBER;
-        }
-        else if(m==10)
-        {
-            return month.OCTOBER;
-        }
-        else if(m==11)
-        {
-            return month.NOVEMBER;
-        }
-        else if(m==12)
-        {
-            return month.DECEMBER;
-        }
-        else
-        {
-            return month.UNKNOWN;
+            case 1:
+                return month.JANUARY;
+            case 2:
+                return month.FEBRUARY;
+            case 3:
+                return month.MARCH;
+            case 4:
+                return month.APRIL;
+            case 5:
+                return month.MAY;
+            case 6:
+                return month.JUNE;
+            case 7:
+                return month.JULY;
+            case 8:
+                return month.AUGUST;
+            case 9:
+                return month.SEPTEMBER;
+            case 10:
+                return month.OCTOBER;
+            case 11:
+                return month.NOVEMBER;
+            case 12:
+                return month.DECEMBER;
+            default:
+                return month.UNKNOWN;
         }
     }
 }
