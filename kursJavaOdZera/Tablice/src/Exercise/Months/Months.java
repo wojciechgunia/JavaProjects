e110a48424acfc5988c2ffdc4fed0748c0eae9b2
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-20 18:18:09 +0200

    JavaOdZera - Tablice - exercise Months

---
+++ kursJavaOdZera/Tablice/src/Exercise/Months/Months.java
@@ -0,0 +1,13 @@
+package Exercise.Months;
+
+public class Months
+{
+    public static void main(String[] args)
+    {
+        month[] Months = {month.JANUARY, month.FEBRUARY, month.MARCH, month.APRIL, month.MAY, month.JUNE, month.JULY, month.AUGUST, month.SEPTEMBER, month.OCTOBER, month.NOVEMBER, month.DECEMBER};
+        for(month m : Months)
+        {
+            System.out.println(m);
+        }
+    }
+}
---
+++ kursJavaOdZera/Tablice/src/Exercise/Months/month.java
@@ -0,0 +1,18 @@
+package Exercise.Months;
+
+public enum month
+{
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER,
+    UNKNOWN
+}
