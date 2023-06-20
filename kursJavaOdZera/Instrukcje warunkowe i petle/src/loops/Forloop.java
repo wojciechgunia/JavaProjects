29fad9e1e0e7f23e1f6e1d9ea246d024d3ae0f72
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-20 14:13:25 +0200

    JavaOdZera - Pętle - for/while

--- kursJavaOdZera/Instrukcje warunkowe i petle/src/loops/Forloop.java
+++ kursJavaOdZera/Instrukcje warunkowe i petle/src/loops/Forloop.java
@@ -4,6 +4,17 @@ public class Forloop
 {
     public static void main(String[] args)
     {
-
+        for(int i=1;i<=20;i++)
+        {
+            if(i%3==0)
+            {
+                continue;
+            }
+            if(i==13)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
     }
 }
--- kursJavaOdZera/Instrukcje warunkowe i petle/src/loops/Whileloops.java
+++ kursJavaOdZera/Instrukcje warunkowe i petle/src/loops/Whileloops.java
@@ -4,6 +4,27 @@ public class Whileloops
 {
     public static void main(String[] args)
     {
+        int i = 1;
+        while(i<=20)
+        {
+            if(i%3==0)
+            {
+                i++;
+                continue;
+            }
+            if(i==13)
+            {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
 
+        i = 12;
+        do
+        {
+            System.out.println(i);
+            i++;
+        } while(i<=10);
     }
 }
