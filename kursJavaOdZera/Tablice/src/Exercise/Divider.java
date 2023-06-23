d5707391f85214579870dcadb8f3f3b3b8159e57
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-23 12:39:26 +0200

    JavaOdZera - JavaAPI - exercise - Divider

---
+++ kursJavaOdZera/Tablice/src/Exercise/Divider.java
@@ -0,0 +1,20 @@
+package Exercise;
+
+import java.util.Random;
+
+public class Divider
+{
+    public static void main(String[] args)
+    {
+        Random r = new Random();
+        int a,b;
+        do
+        {
+            a = r.nextInt(2,101);
+            b = r.nextInt(0, 101);
+        }
+        while(b%a!=0);
+        System.out.println("a = "+a+", b = "+b);
+
+    }
+}
