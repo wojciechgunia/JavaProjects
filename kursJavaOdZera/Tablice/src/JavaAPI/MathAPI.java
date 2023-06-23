2d5a313f8cad7ae355b5162b6079b390c2b316b8
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-23 11:48:57 +0200

    JavaOdZera - JavaAPI - Math

---
+++ kursJavaOdZera/Tablice/src/JavaAPI/MathAPI.java
@@ -0,0 +1,12 @@
+package JavaAPI;
+
+public class MathAPI
+{
+    public static void main(String[] args)
+    {
+        double r = 2;
+        System.out.println(Math.min(4, 2));
+        System.out.println(Math.floor(Math.sqrt(124)));
+        System.out.println(Math.PI*Math.pow(r, 2));
+    }
+}
