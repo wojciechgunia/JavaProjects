26afd91aca0e8c5aaf284e4cced12f6b63869f44
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-23 16:57:50 +0200

    JavaOdZera - Scanner - exercise - GuessGame

---
+++ kursJavaOdZera/Inne tematy i notebook/src/scanner/exercise/GuessGame.java
@@ -0,0 +1,41 @@
+package scanner.exercise;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessGame
+{
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        int number = rand.nextInt(0,101);
+        //System.out.println(number);
+        System.out.println("Welcome to GuessGame!\nGuess what number I thought.\nYou have 5 tries.");
+        Scanner scan = new Scanner(System.in);
+        for(int i = 1;i<=5;i++)
+        {
+            System.out.print("Guess the number: ");
+            int guess = scan.nextInt();
+            if(guess==number)
+            {
+                System.out.println("Win in "+i+" try!");
+                break;
+            }
+            else if(guess>number)
+            {
+                System.out.println("My number is lower.");
+            }
+            else
+            {
+                System.out.println("My number is greater.");
+            }
+            if(i==5)
+            {
+                System.out.println("You lose.");
+            }
+        }
+
+    }
+
+
+}
