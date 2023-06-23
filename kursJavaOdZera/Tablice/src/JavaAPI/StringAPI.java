f477de6344b6097e5da786ae35dc6c049e03a724
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-23 11:55:34 +0200

    JavaOdZera - JavaAPI - String

---
+++ kursJavaOdZera/Tablice/src/JavaAPI/StringAPI.java
@@ -0,0 +1,13 @@
+package JavaAPI;
+
+public class StringAPI
+{
+    public static void main(String[] args)
+    {
+        System.out.println("pawel".charAt(2));
+        System.out.println("pawel".toUpperCase());
+        System.out.println("pawel".length());
+        System.out.println("pawel".indexOf('e'));
+        System.out.println("  pawel  ".trim());
+    }
+}
