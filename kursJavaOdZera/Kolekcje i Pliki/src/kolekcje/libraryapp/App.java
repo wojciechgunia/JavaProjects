b94be946dab9c1514240f80310697ff7a5bf4f7f
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-24 19:46:28 +0200

    JavaOdZera - Kolekcje - Listy - LibraryApp - Adding Authors and Books

--- kursJavaOdZera/Kolekcje i Pliki/src/kolekcje/libraryapp/App.java
+++ kursJavaOdZera/Kolekcje i Pliki/src/kolekcje/libraryapp/App.java
@@ -1,13 +1,12 @@
 package kolekcje.libraryapp;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App
 {
     private final List<Author> authors = new ArrayList<>();
     private final List<Book> books = new ArrayList<>();
+    private final List<Genre> genres = new ArrayList<>();
 
     public App()
     {
@@ -22,6 +21,7 @@ public class App
         books.add(new Book("Guard! Guard!", authors.get(1), Genre.Fantasy));
         books.add(new Book("Death on the Nile", authors.get(2), Genre.Mystery));
         books.add(new Book("The 5 am club", authors.get(3), Genre.Personal_Development));
+        Collections.addAll(genres, Genre.values());
     }
     public void printAuthors()
     {
@@ -32,7 +32,7 @@ public class App
             System.out.println(i+". "+author.getInfo());
             i++;
         }
-        System.out.println("-------------------------------\n");
+        System.out.println("-------------------------------");
     }
     public void printBooks()
     {
@@ -43,7 +43,19 @@ public class App
             System.out.println(i+". "+book.getInfo());
             i++;
         }
-        System.out.println("-----------------------------\n");
+        System.out.println("-----------------------------");
+    }
+
+    public void printGenre()
+    {
+        int i=1;
+        System.out.println("-----------Genre-------------");
+        for(Genre genre : genres)
+        {
+            System.out.println(i+". "+genre);
+            i++;
+        }
+        System.out.println("-----------------------------");
     }
 
     public Author getAuthor(int id)
@@ -56,6 +68,31 @@ public class App
         this.authors.add(author);
     }
 
+    public void addAuthor()
+    {
+        String firstName,lastName;
+        Scanner scan = new Scanner(System.in);
+        try
+        {
+            System.out.print("Author first name: ");
+            firstName=scan.next();
+            System.out.print("Author last name: ");
+            lastName=scan.next();
+            System.out.println("Author "+firstName+" "+lastName+" to add");
+            System.out.print("Are data correct (t/f): ");
+            String correct=scan.next();
+            if(Objects.equals(correct, "t") || Objects.equals(correct, "T"))
+            {
+                this.authors.add(new Author(firstName,lastName));
+            }
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error occured. Try again.");
+        }
+    }
+
     public Book getBook(int id)
     {
         return books.get(id);
@@ -66,6 +103,40 @@ public class App
         this.books.add(book);
     }
 
+    public void addBook()
+    {
+        String title;
+        int genre;
+        int author;
+        Scanner scan = new Scanner(System.in);
+        try
+        {
+            System.out.print("Book title: ");
+            title=scan.nextLine();
+            printAuthors();
+            System.out.print("Author id: ");
+            author=scan.nextInt();
+            if(author>authors.size() || author<1)
+                throw new IllegalArgumentException("Bad value of int");
+            printGenre();
+            System.out.print("Genre id: ");
+            genre=scan.nextInt();
+            if(genre>genres.size() || genre<1)
+                throw new IllegalArgumentException("Bad value of int");
+            System.out.println("Book: "+title+" | "+this.authors.get(author-1).getInfo()+" | "+genres.get(genre-1)+" to add");
+            System.out.print("Are data correct (t/f): ");
+            String correct=scan.next();
+            if(Objects.equals(correct, "t") || Objects.equals(correct, "T"))
+            {
+                this.books.add(new Book(title,this.authors.get(author-1), genres.get(genre-1)));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error occured. Try again.");
+        }
+    }
+
     public static void main(String[] args)
     {
         App app = new App();
@@ -73,23 +144,31 @@ public class App
         boolean running=true;
         while(running)
         {
-            System.out.println("===================================\n0. Exit\n1.Print Authors\n2.Print Books");
-            System.out.print("Choose option: ");
-            int opt;
-            try
-            {
-                opt=scan.nextInt();
-            }
-            catch (Exception e)
-            {
-                opt=0;
-            }
-            switch (opt)
-            {
-                case 1 -> app.printAuthors();
-                case 2 -> app.printBooks();
-                default -> running = false;
-            }
+            running = appMenu(app, scan, running);
         }
+    }
+
+    private static boolean appMenu(App app, Scanner scan, boolean running)
+    {
+        System.out.println("===================================\n0. Exit\n1.Print Authors\n2.Print Books\n3.Add Author\n4.Add Book");
+        System.out.print("Choose option: ");
+        int opt;
+        try
+        {
+            opt= scan.nextInt();
+        }
+        catch (Exception e)
+        {
+            opt=0;
+        }
+        switch (opt)
+        {
+            case 1 -> app.printAuthors();
+            case 2 -> app.printBooks();
+            case 3 -> app.addAuthor();
+            case 4 -> app.addBook();
+            default -> running = false;
+        }
+        return running;
     }
 }
