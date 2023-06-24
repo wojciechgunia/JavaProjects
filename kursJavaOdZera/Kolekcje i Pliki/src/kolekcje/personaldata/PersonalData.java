ebd0ff498d6d849d8f48b4d00b6b21a48ea2456b
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-24 20:21:48 +0200

    JavaOdZera - Kolekcje - Zbiory - PersonalData

--- kursJavaOdZera/Kolekcje i Pliki/src/kolekcje/personaldata/PersonalData.java
+++ kursJavaOdZera/Kolekcje i Pliki/src/kolekcje/personaldata/PersonalData.java
@@ -1,20 +1,18 @@
 package kolekcje.personaldata;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PersonalData
 {
     public static void main(String[] args)
     {
-        List<Person> persons = new ArrayList<>();
+        Set<Person> persons = new HashSet<>();
 
         persons.add(new Person("Wojciech",21,true,Gender.MALE));
         persons.add(new Person("Anna",19,false,Gender.FEMALE));
         persons.add(new Person("Adam",20,false,Gender.MALE));
-        persons.add(new Person("Adam",22,true,Gender.MALE));
-
-        persons.remove(2);
+        persons.add(new Person("Adam",20,false,Gender.MALE));
 
         for (Person person : persons)
         {
