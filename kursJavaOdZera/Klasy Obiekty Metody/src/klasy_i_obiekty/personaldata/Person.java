a92fdcc34b7d6875a608314ecd8a73bdf416121f
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-24 10:43:58 +0200

    JavaOdZera - Klasy i Obiekty - This - PersonalData

--- kursJavaOdZera/Klasy Obiekty Metody/src/klasy_i_obiekty/personaldata/Person.java
+++ kursJavaOdZera/Klasy Obiekty Metody/src/klasy_i_obiekty/personaldata/Person.java
@@ -2,22 +2,22 @@ package klasy_i_obiekty.personaldata;
 
 public class Person
 {
-    String name;
-    int age;
-    boolean study;
-    Gender gender;
+    private final String name;
+    private final int age;
+    private final boolean study;
+    private final Gender gender;
 
     public Person(String newName, int newAge, boolean newStudy, Gender newGender)
     {
-        name = newName;
-        age = newAge;
-        study = newStudy;
-        gender = newGender;
+        this.name = newName;
+        this.age = newAge;
+        this.study = newStudy;
+        this.gender = newGender;
 
     }
 
     public String showPerson()
     {
-        return "Name: "+name+"\n"+"Age: "+age+"\n"+"Student: "+study+"\n"+"Gender: "+gender+"\n";
+        return "Name: "+this.name+"\n"+"Age: "+this.age+"\n"+"Student: "+this.study+"\n"+"Gender: "+this.gender+"\n";
     }
 }
