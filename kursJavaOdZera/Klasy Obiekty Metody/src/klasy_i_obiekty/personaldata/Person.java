15cf016f6c529bb838fa78c2e807f0b23b7e71a2
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-06-24 11:48:31 +0200

    JavaOdZera - Klasy i Obiekty - Gettery i Settery - PersonalData

--- kursJavaOdZera/Klasy Obiekty Metody/src/klasy_i_obiekty/personaldata/Person.java
+++ kursJavaOdZera/Klasy Obiekty Metody/src/klasy_i_obiekty/personaldata/Person.java
@@ -1,11 +1,13 @@
 package klasy_i_obiekty.personaldata;
 
+import java.util.Locale;
+
 public class Person
 {
-    private final String name;
-    private final int age;
-    private final boolean study;
-    private final Gender gender;
+    private String name;
+    private int age;
+    private boolean study;
+    private Gender gender;
 
     public Person(String name, int age, boolean study, Gender gender)
     {
@@ -13,11 +15,56 @@ public class Person
         this.age = age;
         this.study = study;
         this.gender = gender;
-
     }
-
-    public String showPerson()
+    public String getPersonalData()
     {
         return "Name: "+this.name+"\n"+"Age: "+this.age+"\n"+"Student: "+this.study+"\n"+"Gender: "+this.gender+"\n";
+    }
+
+    public void setName(String name)
+    {
+        name=name.trim();
+        if(name.length()<3)
+            System.out.println("Błąd! Podano błędne imię.");
+        else
+            this.name = name.substring(0,1).toUpperCase()+name.substring(1);
+    }
+
+    public void setAge(int age)
+    {
+        if(age>=0 && age<=150)
+            this.age = age;
+        else
+            System.out.println("Błąd! Wiek musi być w przedziale [0 ,150].");
+    }
+
+    public void setStudy(boolean study)
+    {
+        this.study = study;
+    }
+
+    public void setGender(Gender gender)
+    {
+        this.gender = gender;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public boolean getStudy()
+    {
+        return this.study;
+    }
+
+    public Gender getGender()
+    {
+        return this.gender;
     }
 }
--- kursJavaOdZera/Klasy Obiekty Metody/src/klasy_i_obiekty/personaldata/PersonalData.java
+++ kursJavaOdZera/Klasy Obiekty Metody/src/klasy_i_obiekty/personaldata/PersonalData.java
@@ -7,9 +7,17 @@ public class PersonalData
         Person wojciech = new Person("Wojciech",21,true,Gender.MALE);
         Person anna = new Person("Anna",19,false,Gender.FEMALE);
         Person adam = new Person("Adam",20,true,Gender.MALE);
-        System.out.println(wojciech.showPerson());
-        System.out.println(anna.showPerson());
-        System.out.println(adam.showPerson());
+
+        System.out.println(wojciech.getPersonalData());
+        System.out.println(anna.getPersonalData());
+
+        adam.setStudy(false);
+        adam.setAge(-2);
+        adam.setAge(32);
+        adam.setName("    ");
+        adam.setName("  Krztsztof    ");
+
+        System.out.println(adam.getPersonalData());
     }
 
 }
