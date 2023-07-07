bc01f70adbf2ace57d270ed4cdbd999d844108e9
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-07-07 13:32:46 +0200

    kursJavaSpring - kursKontroleryRest - Lombok

--- kursJavaSpring/kursKontroleryRest/src/main/java/com/example/kurs/Family.java
+++ kursJavaSpring/kursKontroleryRest/src/main/java/com/example/kurs/Family.java
@@ -1,35 +1,21 @@
 package com.example.kurs;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
 import java.util.UUID;
-
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Family
 {
     private final String uid;
     private String name;
     private List<Member> members;
-
-    public Family(String uid, String name, List<Member> members)
-    {
-        this.uid = uid;
-        this.name = name;
-        this.members = members;
-    }
-
-    public String getUid()
-    {
-        return uid;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
 
     public List<Member> getMembers()
     {
