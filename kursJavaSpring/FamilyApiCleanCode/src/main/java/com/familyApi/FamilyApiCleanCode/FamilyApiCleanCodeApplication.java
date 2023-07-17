a32064b53c084c5cec7ef8f5b12cce58b9f3914e
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-07-17 16:51:28 +0200

    kursJavaSpring - FamilyApiCleanCode - Start

---
+++ kursJavaSpring/FamilyApiCleanCode/src/main/java/com/familyApi/FamilyApiCleanCode/FamilyApiCleanCodeApplication.java
@@ -0,0 +1,13 @@
+package com.familyApi.FamilyApiCleanCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyApiCleanCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyApiCleanCodeApplication.class, args);
+	}
+
+}
---
+++ kursJavaSpring/FamilyApiCleanCode/src/test/java/com/familyApi/FamilyApiCleanCode/FamilyApiCleanCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyApi.FamilyApiCleanCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyApiCleanCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
