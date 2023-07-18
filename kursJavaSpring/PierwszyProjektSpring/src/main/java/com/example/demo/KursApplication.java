f734e38975e22c619b954f3cf825ab052728a7d9
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-07-18 16:03:03 +0200

    kursJavaSpring - PierwszyProjektSpring - End

--- kursJavaSpring/PierwszyProjektSpring/src/main/java/com/example/demo/KursApplication.java
+++ kursJavaSpring/PierwszyProjektSpring/src/main/java/com/example/demo/KursApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
