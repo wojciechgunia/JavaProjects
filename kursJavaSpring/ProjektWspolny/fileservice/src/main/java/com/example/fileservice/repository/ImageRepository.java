1654cacb754a8f95fc053d3f6207fbac88e281da
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-09-05 13:04:16 +0200

    kursJavaSpring - ProjektWspólny - Usuwanie nieużywanych plików

--- kursJavaSpring/ProjektWspolny/fileservice/src/main/java/com/example/fileservice/repository/ImageRepository.java
+++ kursJavaSpring/ProjektWspolny/fileservice/src/main/java/com/example/fileservice/repository/ImageRepository.java
@@ -2,12 +2,17 @@ package com.example.fileservice.repository;
 
 import com.example.fileservice.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Long>
 {
     Optional<ImageEntity> findByUid(String uid);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM image_data WHERE createat < current_date - interval '2 days' and isused = false")
+    List<ImageEntity> findDontUseImages();
 }
--- kursJavaSpring/ProjektWspolny/fileservice/src/main/java/com/example/fileservice/service/ImageService.java
+++ kursJavaSpring/ProjektWspolny/fileservice/src/main/java/com/example/fileservice/service/ImageService.java
@@ -3,13 +3,19 @@ package com.example.fileservice.service;
 import com.example.fileservice.entity.ImageEntity;
 import com.example.fileservice.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
+@EnableScheduling
 @RequiredArgsConstructor
 public class ImageService
 {
     private final ImageRepository imageRepository;
+    private final FTPService ftpService;
 
     public ImageEntity save(ImageEntity imageEntity)
     {
@@ -19,5 +25,21 @@ public class ImageService
     public ImageEntity findByUid(String uid)
     {
         return imageRepository.findByUid(uid).orElse(null);
+    }
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public  void cleanImages()
+    {
+        imageRepository.findDontUseImages().forEach(value ->{
+            try
+            {
+                ftpService.deleteFile(value.getPath());
+                imageRepository.delete(value);
+            }
+            catch (IOException e)
+            {
+                System.out.println("Cant delete "+value.getUid());
+            }
+        });
     }
 }
