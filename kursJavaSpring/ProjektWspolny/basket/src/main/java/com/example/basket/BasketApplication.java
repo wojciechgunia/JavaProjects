15458480af12a1b659cc4b0c2495f8790204f06d
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-09-26 13:25:46 +0200

    kursJavaSpring - ProjektWspólny - Utworzenie beana Rest Template

--- kursJavaSpring/ProjektWspolny/basket/src/main/java/com/example/basket/BasketApplication.java
+++ kursJavaSpring/ProjektWspolny/basket/src/main/java/com/example/basket/BasketApplication.java
@@ -2,12 +2,21 @@ package com.example.basket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BasketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasketApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder)
+	{
+		return builder.build();
 	}
 
 }
