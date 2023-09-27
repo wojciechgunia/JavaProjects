c928f0963853337651deb8ad21305fa4463d1ac4
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-09-27 13:32:27 +0200

    kursJavaSpring - ProjektWspólny - Podpięcie usługi pod server discovery

--- kursJavaSpring/ProjektWspolny/basket/src/main/java/com/example/basket/BasketApplication.java
+++ kursJavaSpring/ProjektWspolny/basket/src/main/java/com/example/basket/BasketApplication.java
@@ -3,10 +3,12 @@ package com.example.basket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BasketApplication {
 
 	public static void main(String[] args) {
