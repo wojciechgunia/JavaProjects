24a89cab4f3db387f10adefbc555464547bdd332
Wojciech Gunia <wojciech.gunia@outlook.com>
2023-10-04 09:19:43 +0200

    kursJavaSpring - ProjektWspólny - Utworzenie Exception-ów oraz obsługa błędów

--- kursJavaSpring/ProjektWspolny/order/src/main/java/com/example/order/fasade/OrderController.java
+++ kursJavaSpring/ProjektWspolny/order/src/main/java/com/example/order/fasade/OrderController.java
@@ -2,15 +2,18 @@ package com.example.order.fasade;
 
 import com.example.order.entity.DeliverDTO;
 import com.example.order.entity.OrderDTO;
+import com.example.order.exceptions.BasketDontExistException;
+import com.example.order.exceptions.EmptyBasketException;
+import com.example.order.exceptions.PayUException;
+import com.example.order.exceptions.UknowDeliveryTypException;
 import com.example.order.mediator.OrderMediator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.coffeecode.entity.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +28,33 @@ public class OrderController
     public ResponseEntity<?> createOrder(@RequestBody OrderDTO order, HttpServletResponse response, HttpServletRequest request)
     {
         return orderMediator.createOrder(order, request, response);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BasketDontExistException.class)
+    public Response handleValidationExceptions(BasketDontExistException ex)
+    {
+        return new Response("Basket don't exist");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(EmptyBasketException.class)
+    public Response handleValidationExceptions(EmptyBasketException ex)
+    {
+        return new Response("Basket is empty");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(PayUException.class)
+    public Response handleValidationExceptions(PayUException ex)
+    {
+        return new Response("PayU service error, please try later");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UknowDeliveryTypException.class)
+    public Response handleValidationExceptions(UknowDeliveryTypException ex)
+    {
+        return new Response("Delivery type don't exist");
     }
 }
